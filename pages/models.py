31172b5bf9436da30cf1b68c77637e07ef8a9b4d
EngiBarnaby <calnikov13@gmail.com>
2020-04-28 22:43:32 +0300

    Добавил первую модель(Post) и отображение все записей в ней. Так же создал base.html для будущих html файлов.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    body = models.TextField()
+    slug = models.SlugField(unique_for_date='publish')
+    image = models.ImageField(null=True, blank=True)
+    publish = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    all_post = Post.objects.all()
+    context = {'all_post' : all_post}
+    return render(request, "pages/post/post_list.html", context)
