6439363f63a960de90c49a657705eefbc988699b
EngiBarnaby <calnikov13@gmail.com>
2020-04-30 22:14:36 +0300

    Добавлена пагинация и стили для нескольких элементов

--- pages/admin.py
+++ pages/admin.py
@@ -4,3 +4,4 @@ from .models import *
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'publish')
+    prepopulated_fields = {'slug': ('title',)}
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 def post_list(request):
     all_post = Post.objects.all()
-    context = {'all_post' : all_post}
+    pages = Paginator(all_post, 2)
+
+    if "page" in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+
+    page = pages.get_page(page_num)
+
+    context = {'all_post' : page.object_list, 'page' : page}
     return render(request, "pages/post/post_list.html", context)
